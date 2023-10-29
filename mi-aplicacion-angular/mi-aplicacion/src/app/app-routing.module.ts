eea8bfaa0fa09d0320bcea34f37e9227641c7845
RoccoNicoletti <roccolucianonicoletti@gmail.com>
2023-10-29 14:59:58 -0300

    Prueba enrutadores y cracion de componentes

--- mi-aplicacion-angular/mi-aplicacion/src/app/app-routing.module.ts
+++ mi-aplicacion-angular/mi-aplicacion/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//import { EquiposComponent } from './equipos/equipos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ // {path: 'equipos', component: EquiposComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- mi-aplicacion-angular/mi-aplicacion/src/app/app.module.ts
+++ mi-aplicacion-angular/mi-aplicacion/src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SeccionesComponent } from './secciones/secciones.component';
+import { ResultadosComponent } from './resultados/resultados.component';
+import { JugadoresComponent } from './jugadores/jugadores.component';
+import { SobreNosotrosComponent } from './sobre-nosotros/sobre-nosotros.component';
+///import { EquiposComponent } from './equipos/equipos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SeccionesComponent
+    SeccionesComponent,
+    ResultadosComponent,
+    JugadoresComponent,
+    SobreNosotrosComponent,
+    //EquiposComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
