64759cab6841691ce282e478326bbd1c39b1c5b6
Moya Ramiro <moya04.ramiro@gmail.com>
2023-11-06 16:44:25 -0300

    cambiosEnElType

--- mi-aplicacion-angular/mi-aplicacion/src/app/types/User.ts
+++ mi-aplicacion-angular/mi-aplicacion/src/app/types/User.ts
@@ -1,7 +1,12 @@
+import { Player } from "./Players";
+import { Team } from "./Teams";
+
 export class User {
     userId? : number;
     userName? : string;
     password? : string;
     email? : string;
     dni? : string;
+    teamFavouriteList? : Array<Team> | null = null;
+    playersFavouriteList? : Array<Player> | null = null;
 }
