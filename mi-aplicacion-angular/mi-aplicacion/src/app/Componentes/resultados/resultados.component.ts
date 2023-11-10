2190dabd79ce25a037e3c4d82f0bc3e7650f882d
RoccoNicoletti <roccolucianonicoletti@gmail.com>
2023-11-10 12:44:34 -0300

    Muestra de resultados con su respectivo css

--- mi-aplicacion-angular/mi-aplicacion/src/app/Componentes/resultados/resultados.component.ts
+++ mi-aplicacion-angular/mi-aplicacion/src/app/Componentes/resultados/resultados.component.ts
@@ -1,11 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Game } from 'src/app/types/Games';
+import { GamesService } from 'src/app/Servicios/Games/games.service';
 
 @Component({
   selector: 'app-resultados',
   templateUrl: './resultados.component.html',
   styleUrls: ['./resultados.component.css']
 })
-export class ResultadosComponent {
-  constructor(private route: ActivatedRoute){ }
+export class ResultadosComponent implements OnInit {
+
+  public GamesList: Game[] = [];
+  public select:Game| null=null;
+  public atribute=false;
+  public count=1;
+
+
+  constructor( private JuegosService: GamesService, private route: ActivatedRoute) { }
+
+
+  ngOnInit(): void {
+    this.getAllGames(1);
+    console.log(this.route.paramMap)
+    
+  }
+
+  nextPage(): void {
+    if(this.count <= 209){
+      this.count = this.count+1;
+      
+      this.getAllGames(this.count);
+      console.log(this.count)
+    }
+    
+  }
+
+  backPage(): void{
+    if(this.count>1){
+      this.count = this.count-1;
+
+      this.getAllGames(this.count);
+      console.log(this.count)
+    }
+    
+  }
+  
+
+  getAllGames(i : number) {
+    return this.JuegosService.getAllGames(i).subscribe((g:Game[] | any) => {
+
+      console.log(g.data);
+      this.GamesList = this.filterGames(g.data);
+      console.log(this.GamesList);
+    })
+  }
+  //filtra los jugadores para dejar en el array los jugadores que no tengan atributos vacios
+  filterGames(array:Game[]) {
+    return array.filter(game =>
+      Object.values(game).some(value => value !== null)
+    );
+  }
+
+
+
+  showAtributes(game:Game){
+
+    if(this.select===game){
+      this.select=null;
+      this.atribute=false;
+    }
+    else{
+      this.select=game;
+      this.atribute=true;
+    }
+
+  }
+
+
 }
+
+
+
+
+
--- mi-aplicacion-angular/mi-aplicacion/src/app/Servicios/Games/games.service.ts
+++ mi-aplicacion-angular/mi-aplicacion/src/app/Servicios/Games/games.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Game } from 'src/app/types/Games';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GamesService {
 
-  constructor() { }
+  private games: Game[]=[]
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllGames(i:Number){
+
+    const datos=this.httpClient.get(`https://www.balldontlie.io/api/v1/games?page=${i}`)
+    return datos;
+  }
 }
+
+
+export class gamesService {
+  private gamesList = new Array<Game>()
+  constructor() { }
+
+  add(game: Game) {
+    this.gamesList.push(game)
+  }
+
+  remove(game: Game) {
+    this.gamesList;
+  }
+
+}
+
+
