edd70d1c202cd137cb4cfdfb14ca62f236ebed7d
Moya Ramiro <moya04.ramiro@gmail.com>
2023-11-23 16:25:23 -0300

    correccion de no cerrar sesion al recargar pagina y problemas con la userID

--- mi-aplicacion-angular/mi-aplicacion/src/app/Servicios/LoginRegister/login-register.service.ts
+++ mi-aplicacion-angular/mi-aplicacion/src/app/Servicios/LoginRegister/login-register.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { last } from 'rxjs';
 import { User } from 'src/app/types/User';
 
 @Injectable({
@@ -7,54 +8,62 @@ import { User } from 'src/app/types/User';
 export class LoginRegisterService {
 
   constructor() { }
-  
-  
-  private registeredUsers: User[] = []
-  private activeUser : User = {};
 
-  saveUsers(users : Array<User>){
+
+  private registeredUsers: User[] = []
+  private activeUser: User = {};
+
+  saveUsers(users: Array<User>) {
     localStorage.setItem("usuarios", JSON.stringify(users));
   }
 
-  returnUsersFromLocalStorage(): Array<User>{
+  returnUsersFromLocalStorage(): Array<User> {
     const userData = localStorage.getItem("usuarios");
 
-    if(userData) {
+    if (userData) {
       return JSON.parse(userData) as User[];
     } else {
       return [];
     }
   }
 
-  setActiveUser(user : User){
+  setActiveUser(user: User) {
     this.activeUser = user;
+    localStorage.setItem('user', JSON.stringify(user));
   }
 
-  getActiveUser(){
+  getActiveUser() {
+    const userString = localStorage.getItem('user');
+
+    if (userString) {
+      const user: User = JSON.parse(userString);
+      this.activeUser = user;
+    }
+
     return this.activeUser;
   }
 
-  isAciveUser(){
-    if(this.activeUser && Object.keys(this.activeUser).length > 0){
+  isAciveUser() {
+    if (this.activeUser && Object.keys(this.activeUser).length > 0) {
       return true;
     } else {
       return false;
     }
   }
 
-  getUser (username : string | undefined | null): User | null{
+  getUser(username: string | undefined | null): User | null {
     this.registeredUsers = this.returnUsersFromLocalStorage();
 
-    const userFound = this.registeredUsers.find(user=>user.userName === username);
+    const userFound = this.registeredUsers.find(user => user.userName === username);
 
-    if(userFound){
+    if (userFound) {
       return userFound
     } else {
       return null;
     }
   }
 
-  updateUser(updatedUser : User){
+  updateUser(updatedUser: User) {
     this.registeredUsers = this.returnUsersFromLocalStorage();
 
     this.registeredUsers = this.registeredUsers.map(user => user.userId === updatedUser.userId ? updatedUser : user);
@@ -62,12 +71,12 @@ export class LoginRegisterService {
     this.saveUsers(this.registeredUsers);
   }
 
-  addUser(user : User){
+  addUser(user: User) {
     this.registeredUsers = this.returnUsersFromLocalStorage();
     //retorna false si existen usuarios con mismo mail, dni o username iguales, retorna true si no hay users con coincidencias
-    
 
-    if(this.validateRegister(user)){
+
+    if (this.validateRegister(user)) {
       console.log("no se ha podido hacer el registro.... usuarios con mail, dni o username repetidos")
       return false;
     } else {
@@ -77,26 +86,35 @@ export class LoginRegisterService {
       console.log(this.returnUsersFromLocalStorage());
       return true;
     }
-    
+
   }
 
-  validateRegister(user : User){
+  validateRegister(user: User) {
     this.registeredUsers = this.returnUsersFromLocalStorage();
     //retorna true si existen usuarios con mismo mail, dni o username iguales, retorna false si no hay users con coincidencias
-    return this.registeredUsers.some(userRegistrado=>userRegistrado.email == user.email || userRegistrado.dni == user.dni || userRegistrado.userName == user.userName)
+    return this.registeredUsers.some(userRegistrado => userRegistrado.email == user.email || userRegistrado.dni == user.dni || userRegistrado.userName == user.userName)
   }
 
-  getNextId(): number{
-    if(this.registeredUsers.length > 0){
-      return this.registeredUsers.length;
+  getNextId(): number {
+    const users = this.returnUsersFromLocalStorage();
+
+    if (users && users.length > 0) {
+      const lastUser = users[users.length - 1];
+
+      if (lastUser && lastUser.userId !== undefined) {
+        return lastUser.userId + 1;
+      } else {
+        console.error('Error: lastUser.userId es undefined.');
+        return 1;
+      }
     } else {
-      return 0;
+      return 1;
     }
   }
 
-  validateUser(userName : string | undefined | null, password : string | undefined | null){
+  validateUser(userName: string | undefined | null, password: string | undefined | null) {
     this.registeredUsers = this.returnUsersFromLocalStorage();
 
-    return this.registeredUsers.find(user=> user.userName == userName && user.password == password); 
+    return this.registeredUsers.find(user => user.userName == userName && user.password == password);
   }
 }
